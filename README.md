3dff4a3c37db756180a23ec99e126663713b32a0
mohamed2003-cmd <118134131+mohamed2003-cmd@users.noreply.github.com>
2023-07-20 01:57:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-trucjx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-trucjx)
---
+++ index.ts
@@ -0,0 +1,19 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('bonjour tout le monde');
+let culcu1: number;
+let culcu2: number;
+culcu1 = 10;
+culcu2 = 2;
+console.log(culcu1 + culcu2);
+console.log(culcu1 - culcu2);
+console.log(culcu1 * culcu2);
+console.log(culcu1 / culcu2);
+console.log(culcu1 + culcu1);
+let culcu3, culcu4, culcu5: number;
+culcu3 = 5;
+culcu4 = 100;
+culcu5 = 2;
+console.log((culcu3 * culcu4) / culcu5);
